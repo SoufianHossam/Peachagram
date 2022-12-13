6f2649592275b8c2187bc8a56869bfc95debb291
Soufian <soufian.el.hossary@gmail.com>
2022-12-13 06:44:04 +0200

    Initial Commit

---
+++ Peachagram/PeachagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeachagramApp.swift
+//  Peachagram
+//
+//  Created by Soufian Hossam on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct PeachagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
